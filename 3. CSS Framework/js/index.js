19a926945135e694e640f0eb1fae8836db04f461
marconaef <98390695+marconaef@users.noreply.github.com>
2022-11-15 21:50:33 +0100

    Day 3

--- 3. CSS Framework/js/index.js
+++ 3. CSS Framework/js/index.js
@@ -2,6 +2,39 @@
 $(function () {
     
 
+/* $("#myTable tbody tr").click(function (e) { 
+    e.preventDefault();
+    var id = $(this).attr("data-id");
+    console.log(id);
+}); */
 
+$.getJSON("data/daten.json",
+    function (response) {
+        console.log(response);
+    }
+);
+
+$("#myTable .tank").click(function (e) { 
+    e.preventDefault();
+    var id = $(this).parent().parent().attr("data-id");
+    console.log("Tanke: " + id);
+});
+
+$("#myTable .edit").click(function (e) { 
+    e.preventDefault();
+    var id = $(this).parent().parent().attr("data-id");
+    console.log("Edit: " + id);
+});
+
+$("#myTable .delete").click(function (e) { 
+    e.preventDefault();
+    var id = $(this).parent().parent().attr("data-id");
+    console.log("Delete: " + id);
+});
+
+$(".btn").click(function (e) { 
+    e.preventDefault();
+    console.log("ADD");
+});
 
 });
