5d57f1ffdd192f7bc74b03e64b1abb87fdb004e9
marconaef <98390695+marconaef@users.noreply.github.com>
2022-11-18 09:35:10 +0100

    API

--- 3. CSS Framework/js/index.js
+++ 3. CSS Framework/js/index.js
@@ -8,33 +8,46 @@ $(function () {
     console.log(id);
 }); */
 
-$.getJSON("data/daten.json",
-    function (response) {
-        console.log(response);
-    }
-);
-
-$("#myTable .tank").click(function (e) { 
-    e.preventDefault();
-    var id = $(this).parent().parent().attr("data-id");
-    console.log("Tanke: " + id);
-});
-
-$("#myTable .edit").click(function (e) { 
-    e.preventDefault();
-    var id = $(this).parent().parent().attr("data-id");
-    console.log("Edit: " + id);
-});
-
-$("#myTable .delete").click(function (e) { 
-    e.preventDefault();
-    var id = $(this).parent().parent().attr("data-id");
-    console.log("Delete: " + id);
-});
-
 $(".btn").click(function (e) { 
     e.preventDefault();
     console.log("ADD");
 });
+
+
+var template = $("#template").html();
+//console.log(template);
+//Template compillieren
+var handlebars_template = Handlebars.compile(template);
+
+$.getJSON("data/daten.json",
+    function (response) {
+        //Gibt alle Cars Objekte im Log aus
+        console.log(response);
+
+        //console.log(handlebars_template(response));
+        $("#myTable tbody").html(handlebars_template(response));
+        $("#myTable .tank").click(function (e) { 
+            e.preventDefault();
+            var id = $(this).parent().parent().attr("data-id");
+            console.log("Tanke: " + id);
+        });
+        
+        $("#myTable .edit").click(function (e) { 
+            e.preventDefault();
+            var id = $(this).parent().parent().attr("data-id");
+            console.log("Edit: " + id);
+        });
+        
+        $("#myTable .delete").click(function (e) { 
+            e.preventDefault();
+            var id = $(this).parent().parent().attr("data-id");
+            console.log("Delete: " + id);
+        });
+    }
+);
+
+    $.getJSON("api.php", function(response) {
+        console.log(response);
+    })
 
 });
