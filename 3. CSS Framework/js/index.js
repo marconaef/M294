a36668a2efde97f5542291bdedd0db785916dbb1
marconaef <98390695+marconaef@users.noreply.github.com>
2022-11-21 09:59:43 +0100

    CRUD

--- 3. CSS Framework/js/index.js
+++ 3. CSS Framework/js/index.js
@@ -12,17 +12,42 @@ $(function () {
     e.preventDefault();
     console.log("ADD");
     // Beides hier machen um Zuerst Html einzulesen
-    $("#modal_content").load("./pages/formular.html", function(){
-        $.getScript("./js/formular.js")
-    })
+    $("#modal_content").load("./pages/formular.html", function () {
+      $.getScript("./js/formular.js");
+    });
   });
 
+  showList();
+
+  /*     $.getJSON("api.php", function(response) {
+        console.log(response);
+    }) */
+
+  // Schreiben von Daten (insert und Update)
+  $.ajax({
+    type: "POST",
+    //Update eines datensatzes mit mitgabe der id ansonsten ist wird ein datensatz hinzugefügt
+    url: "api.php?id=2",
+    data: {
+      name: "Harald",
+      kraftstoff: "diesel",
+      farbe: "pink",
+    },
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+    },
+  });
+});
+
+function showList() {
   var template = $("#template").html();
   //console.log(template);
   //Template compillieren
   var handlebars_template = Handlebars.compile(template);
-
-  $.getJSON("api.php", function (response) {
+  //mit ? kann man get Variablen definieren
+  // mit id=4 wird nur der 4 datensatz ausgewählt
+  $.getJSON("api.php?test=test&ich=jaja", function (response) {
     //Gibt alle Cars Objekte im Log aus
     console.log(response);
 
@@ -44,10 +69,14 @@ $(function () {
       e.preventDefault();
       var id = $(this).parent().parent().attr("data-id");
       console.log("Delete: " + id);
+      $.ajax({
+        type: "DELETE",
+        url: "api.php?id=" + id,
+        dataType: "json",
+        success: function (response) {
+          showList();
+        }
+      });
     });
   });
-
-  /*     $.getJSON("api.php", function(response) {
-        console.log(response);
-    }) */
-});
+}
